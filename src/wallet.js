e95ed150b9c3878430ca52a48bb732f937823c74
Gossamer <hello@joculator.io>
2017-11-26 17:05:37 -0500

    parse float grr

--- src/wallet.js
+++ src/wallet.js
@@ -81,7 +81,7 @@ class Wallet extends Command {
   }
 
   moveCoins(opts) {
-    return client.move(opts.from, opts.to, opts.amount);
+    return client.move(opts.from, parseFloat(opts), opts.amount);
   }
 
   sendCoinsToAddress(address, amount) {
